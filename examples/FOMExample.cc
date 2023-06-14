16fc0895dbd9bc8e00d909dbd0e531c904fe5698
ChrisW84-HRW <83717974+ChrisW84-HRW@users.noreply.github.com>
2023-06-14 15:45:23 -0400

    Update FOMExample.cc because of new class FiguresOfMerit

--- examples/FOMExample.cc
+++ examples/FOMExample.cc
@@ -24,7 +24,7 @@
 
 #include "latticetester/FlexTypes.h"
 #include "latticetester/EnumTypes.h"
-#include "latticetester/BasisConstruction.h"
+#include "latticetester/FiguresOfMerit.h"
 #include "latticetester/Util.h"
 #include "latticetester/ParamReader.h"
 #include "latticetester/IntLattice.h"
@@ -41,129 +41,45 @@
 using namespace LatticeTester;
 
 int main() {
-  //! Reading a matrix to use the normalizer class
-  int64_t min_dim = 0, max_dim = 10;
+  // Reading a matrix to use the normalizer class
+  int64_t max_dim = 10;
   Int m(1021);     // Modulus m = 1021
   Int a; 
-  a = m /5;
+  IntVec t;
+  double f;
+  a = 57;
   long dim;
   dim = max_dim;
   //Build a Korobov lattice
   Rank1Lattice<Int, Real> *lat;
   lat = new Rank1Lattice<Int, Real>(m, a, dim);
   lat->buildBasis(dim);   
-  ProjConstructType pctype;
-  //Choose which projection construction is used LLLPROJ or UPPERTRIPROJ
-  pctype = UPPERTRIPROJ;
   
-  IntLattice<Int, Real> *proj; // Another IntLattice to store projections
-  
-  double merit1 = 1.0, merit2 = 1.0;
+  FiguresOfMerit<Int> fom(m, max_dim);
+  fom.calculNorma(*lat, max_dim);
+  //fom.set_ProjConstructType(LLLPROJ);
+  fom.set_PreReductionType(LLL);
+  //fom.set_lowerbound(0.5);
 
-  IntMat projBasis; //Matrix for basis of projection
-  
-  Reducer<Int, Real> *red = new Reducer<Int, Real>(max_dim);   
-
-  // The variables specific to the construction of a figure of merit
-   WeightsUniform weights(1.0); // This just puts a weight of 1 to everything
-   
-  // EXAMPLE 1: LOOK AT PROJECTIONS TO ALL COORDINATES //
-   
-  // Creates an iterator over a set of projections.
-  // It contains all sets of size at least min_dim + 2 and at most max_dim. 
-  // within the coordinates {min_dim, min_dim + 1, ..., max_dim - 1}
-  // Example (2, 4, 0, 3) would be all subsets of {0, 1, 2, 3} of cardinaly between 2 and 4.
-   //CoordinateSets::FromRanges coord(min_dim+2, max_dim, min_dim, max_dim - 1);     
-   CoordinateSets::FromRanges allcoord(min_dim+2, max_dim, min_dim, max_dim - 1);      
-   
-      
-  // Loop over the selected set of projections.
-  for(auto it = allcoord.begin(); it != allcoord.end(); it++){
-    // Computing a basis for the projection, using chosen projection type 	  
-	BasisConstruction<Int>::projectionConstruction(lat->getBasis(), projBasis, *it, m, pctype);	
-	proj = new IntLattice<Int, Real> (projBasis, m, projBasis.NumCols());
-    
-    //! Computing the shortest vector in the lattice spanned by matrix
-    proj->updateVecNorm();
-    proj->sort(0);
-    
-    red->redBKZ(proj->getBasis());
-  
-    red->shortestVector(*proj);
-    double shortest = NTL::conv<double>(red->getMinLength());
-
-    // Instantiating the normalizers
-    // The prefered way of doing this is described in Normalizer documentation
-    double log_density=(double)(-log(abs(NTL::determinant(proj->getBasis()))));
-    Normalizer* norma = new NormaBestLat(log_density, max_dim);
-
-    // Computing the figure of merit for this projection
-    double merit = weights.getWeight(*it) * shortest/norma->getBound((*it).size());
-    // Testing if it is the minimum as of now
-    if (merit < merit1) merit1 = merit;
-    delete norma;
-    norma = new NormaBestBound(log_density, max_dim);
-    merit = weights.getWeight(*it) * shortest/norma->getBound((*it).size());
-    if (merit < merit2) merit2 = merit;
-    delete norma;
-  }
-
-  //! Printing the results in three simple lines
-  std::cout << "CASE 1: Look at all possible subsets with cardinality > 1:" << "\n";
-  std::cout << "Figure of merit with BestLat: " << merit1 << "\n";
-  std::cout << "Figure of merit with BestBound: " << merit2 << std::endl;
+  t.SetLength(3);
+  t[0] = 3;
+  t[1] = 5;
+  t[2] = 8;
+  //Calculate figure for t = [3 5 8];
+  f = fom.computeMeritM(*lat, t);
+  std::cout << "CASE 1: Look at t = [3,5,8]:" << "\n";
+  std::cout << "Figure of merit FoM M is: " << f << "\n";
+  std::cout << "\n";
+  t.SetLength(2);
+  t[0] = 2;
+  t[1] = 4;  
+  std::cout << "CASE 2: Look at t = [2,4]:" << "\n";
+  std::cout << "Figure of merit with BestLat: " << f << "\n";
+  std::cout << "\n";
+  //std::cout << "Figure of merit with BestBound: " << merit2 << std::endl;
   std::cout << "Figures of merit are different for different normalizers,"
                " weights and projections choices\n";
   std::cout << "\n";
-
-  // ============================================================================
-
-  // EXAMPLE 2: LOOK AT TWO- AND THREE-DIMENSIONAL PROJECTIONS ONLY
-  
-  // Reset figures of merit and basis of projection
-  merit1 = 1.0, merit2 = 1.0;
-  projBasis.resize(0,0);
-  
-  // Pick projections
-  CoordinateSets::FromRanges ldcoord(2, 3, min_dim, max_dim - 1);  
-  
-  // Loop over the selected set of projections.
-  for(auto it = ldcoord.begin(); it != ldcoord.end(); it++){
-    // Computing a basis for the projection, using chosen projection type.
-    BasisConstruction<Int>::projectionConstruction(lat->getBasis(), projBasis, *it, m, pctype);
-	proj = new IntLattice<Int, Real> (projBasis, m, projBasis.NumCols());
-    //! Computing the shortest vector in the lattice spanned by matrix
-    proj->updateVecNorm();
-    proj->sort(0);
-    
-    red->redBKZ(proj->getBasis());
-  
-    red->shortestVector(*proj);
-    double shortest = NTL::conv<double>(red->getMinLength());
-
-    // Instantiating the normalizers
-    // The prefered way of doing this is described in Normalizer documentation
-    double log_density=(double)(-log(abs(NTL::determinant(proj->getBasis()))));
-    Normalizer* norma = new NormaBestLat(log_density, max_dim);
-
-    // Computing the figure of merit for this projection
-    double merit = weights.getWeight(*it) * shortest/norma->getBound((*it).size());
-    // Testing if it is the minimum as of now
-    if (merit < merit1) merit1 = merit;
-    delete norma;
-    norma = new NormaBestBound(log_density, max_dim);
-    merit = weights.getWeight(*it) * shortest/norma->getBound((*it).size());
-    if (merit < merit2) merit2 = merit;
-    delete norma;
-  }
-  
-  //! Printing the results in three simple lines
-  std::cout << "CASE 2: Only look at all two- and three-dimensional projections:" << "\n";
-  std::cout << "Figure of merit with BestLat: " << merit1 << "\n";
-  std::cout << "Figure of merit with BestBound: " << merit2 << std::endl;
-  std::cout << "Figures of merit are different for different normalizers,"
-               " weights and projections choices\n";
-  
 
 
   return 0;
