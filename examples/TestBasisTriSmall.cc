289c0405d4a8c71c280cffb4acb706b361c9ffa3
pierre <lecuyer@iro.umontreal.ca>
2025-02-02 11:38:18 -0500

    Updated TestBasisTriSmall + results

--- examples/TestBasisTriSmall.cc
+++ examples/TestBasisTriSmall.cc
@@ -25,45 +25,47 @@ const int64_t dimensions[maxNumSizes] = { 5, 10, 15, 20, 25, 30, 35 };
 // Runs a test for dim = dimensions[d], for triangular basis constructions.
 // Only basis0 needs to be initialized; the other matrices are used only for copy.
 template<typename Int, typename Real>
-void triangularBasesTrace(const Int &m, int64_t d, int64_t dim, IntMat &basis0,
+void triangularBasesTrace(const Int &m, int64_t d, int64_t dim, double delta, IntMat &basis0,
 IntMat &basis1, IntMat &basis2, IntMat &basisdual) {
    NTL::Vec<Real> sqlen; // Cannot be global variable because it depends on Real.
    sqlen.SetLength(1);
 
    // We first apply LLL to basis0.
    std::cout << "*************************************************************\n";
-   std::cout << "primal basis before LLL5:\n" << basis0 << "\n";
-   LLLConstruction0<Int, Real>(basis0, 0.5, dim, dim, &sqlen);
-   std::cout << "primal basis after LLL5:\n" << basis0 << "\n";
+   std::cout << "Number of dimensions: " << dim << "\n\n";
+   std::cout << "primal basis before LLL:\n" << basis0 << "\n";
+   LLLConstruction0<Int, Real>(basis0, delta, dim, dim, &sqlen);
+   std::cout << "primal basis after LLL:\n" << basis0 << "\n";
    std::cout << "shortest vector length: " << sqrt(conv<double>(sqlen[0])) << "\n\n";
 
    CopyPartMat<IntMat>(basis1, basis0, dim, dim);  // Copy basis0 to basis1.
    lowerTriangularBasis<Int>(basis2, basis1, m, dim, dim);
-   std::cout << "lower-triangular primal basis before LLL5:\n" << basis2 << "\n";
+   std::cout << "lower-triangular primal basis before LLL:\n" << basis2 << "\n";
    mDualLowerTriangular(basisdual, basis2, m, dim);
-   std::cout << "upper-triangular basisdual before LLL5:\n" << basisdual << "\n";
-   LLLConstruction0<Int, Real>(basisdual, 0.5, dim, dim, &sqlen);
-   std::cout << "basisdual after LLL5:\n" << basisdual << "\n";
+   std::cout << "upper-triangular basisdual before LLL:\n" << basisdual << "\n";
+   LLLConstruction0<Int, Real>(basisdual, delta, dim, dim, &sqlen);
+   std::cout << "basisdual after LLL:\n" << basisdual << "\n";
    std::cout << "shortest vector length in m-dual: " << sqrt(conv<double>(sqlen[0])) << "\n\n";
 
    CopyPartMat<IntMat>(basis1, basis0, dim, dim);  // Copy basis0 to basis1.
    upperTriangularBasis<Int>(basis2, basis1, m, dim, dim);
-   std::cout << "upper-triangular primal basis before LLL5:\n" << basis2 << "\n";
+   std::cout << "upper-triangular primal basis before LLL:\n" << basis2 << "\n";
    mDualUpperTriangular(basisdual, basis2, m, dim);
-   std::cout << "lower-triangular basisdual before LLL5:\n" << basisdual << "\n";
-   LLLConstruction0<Int, Real>(basisdual, 0.5, dim, dim, &sqlen);
-   std::cout << "basisdual after LLL5:\n" << basisdual << "\n";
+   std::cout << "lower-triangular basisdual before LLL:\n" << basisdual << "\n";
+   LLLConstruction0<Int, Real>(basisdual, delta, dim, dim, &sqlen);
+   std::cout << "basisdual after LLL:\n" << basisdual << "\n";
    std::cout << "shortest vector length in m-dual: " << sqrt(conv<double>(sqlen[0])) << "\n\n";
 }
 
 // Testing loop. The `IntMat` and `Rank1Lattice` objects are created only once.
 template<typename Int, typename Real>
-void testLoop(const Int &m, Int &a, int64_t numSizes) {
+void testLoop(const Int &m, Int &a, int64_t numSizes, double delta) {
    std::string stringTypes;  // To print the selected flexible types.
    strTypes<Int, Real>(stringTypes);  // Functions from FlexTypes
    std::cout << "**************************************************************\n";
    std::cout << "TestBasisTriSmall with m = " << m << ",  a = " << a << "\n";
    std::cout << "Types: " << stringTypes << "\n";
+   std::cout << "LLL wih delta = " << delta << "\n";
    int64_t d, dim;  // Index of dimension, and dimension.
    IntMat basis0, basis1, basis2, basisdual;
    int64_t maxdim = dimensions[numSizes - 1];   // Maximum dimension
@@ -78,21 +80,22 @@ void testLoop(const Int &m, Int &a, int64_t numSizes) {
       basis2.SetDims(dim, dim);  // basis.
       korlat.buildBasis(dim);
       CopyPartMat<IntMat>(basis0, korlat.getBasis(), dim, dim); // Copy korlat basis to basis1.
-      triangularBasesTrace<Int, Real>(m, d, dim, basis0, basis1, basis2, basisdual);
+      triangularBasesTrace<Int, Real>(m, d, dim, delta, basis0, basis1, basis2, basisdual);
    }
 }
 
 int main() {
 
 // Here, `Int` and `Real` are not yet defined, they will be passed as template parameters.
-//NTL::ZZ mm(1021);  // Prime modulus near 2^{10}
-NTL::ZZ mm(1048573);  // Prime modulus near 2^{20}
-// NTL::ZZ mm(1073741827);  // Prime modulus near 2^{30}
-//NTL::ZZ a(73245663);
+NTL::ZZ mm(1021);  // Prime modulus near 2^{10}
+//TL::ZZ mm(1048573);  // Prime modulus near 2^{20}
+//NTL::ZZ mm(1073741827);  // Prime modulus near 2^{30}
+// NTL::ZZ a(73245663);
 NTL::ZZ a(73);
-int64_t numSizes = 1;
+int64_t numSizes = 7;
+double delta = 0.5;
 
-testLoop<NTL::ZZ, double>(mm, a, numSizes);
+testLoop<NTL::ZZ, double>(mm, a, numSizes, delta);
 return 0;
 }
 
