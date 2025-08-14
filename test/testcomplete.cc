42ab7986626fe2ae1db1efdbe2c024de647764d0
ChrisW84-HRW <83717974+ChrisW84-HRW@users.noreply.github.com>
2025-08-14 11:50:38 +0200

    Update testcomplete.cc

--- test/testcomplete.cc
+++ test/testcomplete.cc
@@ -2,7 +2,11 @@
 #include <iostream>
 
 int main() {
+    std::cout << "##########################\nTesting Coordinates.h \n########################## \n \n";
+    int result = system("./testCoordinates");
     std::cout << "##########################\nTesting Util.h \n########################## \n \n";
-    int result = system("./testUtil");
+    result = system("./testUtil");
+    std::cout << "##########################\nTesting Rank1Lattice.h \n########################## \n \n";
+    result = system("./testRank1Lattice");
     return result;
 }
