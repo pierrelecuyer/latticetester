bfca88e46791daa078dec1d98fcbfcb1e9ffce5c
pierre <lecuyer@iro.umontreal.ca>
2025-09-02 16:39:19 -0400

    Improved TestSpectrallCG

--- examples/TestSpectralLCG31.cc
+++ examples/TestSpectralLCG31.cc
@@ -20,15 +20,21 @@ using namespace LatticeTester;
 
 int main() {
    int64_t maxdim(30);  // Maximum dimension of the lattice.
-   NTL::Vec <int64_t> t; // The t-vector for the FOM.
    WeightsUniform weights(1.0);
    Int m(2147483647);   // Modulus.
    NormaBestLat norma(log(m), 1, maxdim);      // Normalization for primal.
    NormaBestLat normaDual(-log(m), 1, maxdim); // Normalization for m-dual.
    ReducerBB<Int, Real> red(maxdim);           // Single ReducerBB.
+   NTL::Vec <int64_t> t; // The t-vector for the FOM, used only for pairs.
+   t.SetLength(2);
+   t[0] = 0;
+   t[1] = maxdim;  // We look at pairs {1,s} for s up to maxdim.
+   IntLattice<Int, Real> proj(m, t.length());
    FigureOfMeritM<Int, Real> fom(t, weights, norma, &red, true); // FoM for dual lattice.
-   fom.setVerbosity(3);
    FigureOfMeritDualM<Int, Real> fomdual(t, weights, normaDual, &red, true); // FoM for dual lattice.
+   fom.setTVector(t, true);
+   fomdual.setTVector(t, true);
+   fom.setVerbosity(3);
    fomdual.setVerbosity(3);
    std::cout << "\nResults from TestSpectralLCG.cc \n";
 
@@ -40,6 +46,12 @@ int main() {
    fom.computeMeritSucc(lcg, 2, maxdim);
    std::cout << "\nDual lattice:\n";
    fomdual.computeMeritSucc(lcg, 2, maxdim);
+   std::cout << "\n=========================\n";
+   std::cout << "LCG-16807, projections over pairs of coordinates \n";
+   std::cout << "\nPrimal lattice:\n";
+   fom.computeMerit(lcg, proj);
+   std::cout << "\nDual lattice:\n";
+   fomdual.computeMerit(lcg, proj);
 
    std::cout << "\n=============================================\n";
    std::cout << "LCG-742938285, with BKZ+BB with default parameters, succ. coordinates \n";
@@ -49,20 +61,13 @@ int main() {
    fom.computeMeritSucc(lcg, 2, maxdim);
    std::cout << "\nDual lattice:\n";
    fomdual.computeMeritSucc(lcg, 2, maxdim);
-
-   std::cout << "\n=============================================\n";
+   std::cout << "\n===========================\n";
    std::cout << "LCG-742938285, projections over pairs of coordinates \n";
-   t.SetLength(2);
-   t[0] = 0;    // We look at successive coordinates in up to t[0] dimensions.
-   t[1] = maxdim;
-   fom.setTVector(t, true);
-   fomdual.setTVector(t, true);
-   IntLattice<Int, Real> proj(m, t.length());
    std::cout << "\nPrimal lattice:\n";
    fom.computeMerit(lcg, proj);
    std::cout << "\nDual lattice:\n";
    fomdual.computeMerit(lcg, proj);
-
+   std::cout << "\nDone";
    return 0;
 }
 
